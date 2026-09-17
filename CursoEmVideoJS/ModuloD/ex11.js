9ac44edd3252bc1fa76722cd5bb6d3492e3782fa
fmvini <viniciusmarrocos7@gmail.com>
2026-09-17 09:49:23 -0300

    add ex

---
+++ CursoEmVideoJS/ModuloD/ex11.js
@@ -0,0 +1,11 @@
+var idade = 22
+if (idade < 16){
+    console.log('Não Vota')
+} else{
+    if (idade < 18){
+        console.log('Voto opcional')
+    }
+    else{
+        console.log('Vota')
+    }
+}
